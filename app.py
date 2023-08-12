98b8b3056bf739a90f277cd0429cc5b70715eb3e
AkbarMaxmaniyozov <maxmaniyozov96@mail.ru>
2023-08-13 01:18:24 +0500

    file
    files

---
+++ app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+from fastai.vision.all import *
+import pathlib
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+import plotly.express as px
+
+# title
+st.title("Transportni klassifikatsiya qiluvchi model (car, boat, airplane)")
+
+#rasmni joylash
+file = st.file_uploader("Rasm yuklash", type=['png', 'jpeg', 'gif', 'svg'])
+if file:
+    st.image(file)
+    #PIL convert
+    img = PILImage.create(file)
+
+    #Modelni yuklash
+    model = load_learner("transport_model.pkl")
+
+    #prediction
+    pred, pred_id, probs = model.predict(img)
+    st.success(pred)
+    st.info(f"Ehtimollik: {probs[pred_id]:.1%}")
+
+    #plotting
+    fig = px.bar(x=probs*100, 
+                 y=model.dls.vocab,
+                title="Klasslar bo'yicha ehtimollik")
+    st.plotly_chart(fig)
